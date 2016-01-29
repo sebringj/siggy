9e04fbe91b2b7f60ed60c8cd646b54dcbd226571
Jason Sebring <mail@jasonsebring.com>
2016-01-28 21:29:33 -0800

    beautify

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(body_parser.json());
-app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.urlencoded({
+	extended: true
+}));
 
 var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
@@ -20,31 +22,35 @@ var postUrl = 'https://' + S3_BUCKET + '.s3.amazonaws.com/'
 
 app.listen(app.get('port'));
 
-app.get('/sign_s3', cors(), function(req, res){
+app.get('/sign_s3', cors(), function(req, res) {
 	if (typeof req.query.key !== 'string' || req.query.key.length <= 3)
-		return res.status(400).json({ err: 'bad request' });
+		return res.status(400).json({
+			err: 'bad request'
+		});
 
 	var date = new Date();
 	date.setTime(Date.now() + (30 * 60 * 1000));
 	var isoDate = date.toISOString();
 
 	var s3Policy = {
-    expiration: isoDate,
-    conditions: [
-      ['starts-with', '$key', prefix],
-      {bucket: S3_BUCKET},
-      {acl: 'public-read'},
-      //['starts-with', '$Content-Type', '*']
-    ]
-  };
+		expiration: isoDate,
+		conditions: [
+			['starts-with', '$key', prefix], {
+				bucket: S3_BUCKET
+			}, {
+				acl: 'public-read'
+			},
+			//['starts-with', '$Content-Type', '*']
+		]
+	};
 
 	// stringify and encode the policy
-  var stringPolicy = JSON.stringify(s3Policy);
-  var base64Policy = Buffer(stringPolicy, 'utf-8').toString('base64');
+	var stringPolicy = JSON.stringify(s3Policy);
+	var base64Policy = Buffer(stringPolicy, 'utf-8').toString('base64');
 
-  // sign the base64 encoded policy
-  var signature = crypto.createHmac('sha1', AWS_SECRET_KEY)
-    .update(new Buffer(base64Policy, 'utf-8')).digest('base64');
+	// sign the base64 encoded policy
+	var signature = crypto.createHmac('sha1', AWS_SECRET_KEY)
+		.update(new Buffer(base64Policy, 'utf-8')).digest('base64');
 
 
 	res.json({
