f5b19626f3b6e93080b4a76a83b5f17ecef9699e
Jason Sebring <mail@jasonsebring.com>
2016-01-28 21:28:40 -0800

    simplified

--- README.md
+++ README.md
@@ -10,23 +10,16 @@ TODO: will integrate in with JWT token permission as a microservice
 
 2. Your S3 bucket needs CORS setup with following XML entry:
 
+
     &lt;?xml version="1.0" encoding="UTF-8"?&gt;
     &lt;CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/"&gt;
-
       &lt;CORSRule&gt;
-
         &lt;AllowedOrigin&gt;*&lt;/AllowedOrigin&gt;
-
         &lt;AllowedMethod&gt;GET&lt;/AllowedMethod&gt;
-
         &lt;AllowedMethod&gt;POST&lt;/AllowedMethod&gt;
-
         &lt;AllowedMethod&gt;PUT&lt;/AllowedMethod&gt;
-
         &lt;AllowedHeader&gt;*&lt;/AllowedHeader&gt;
-
       &lt;/CORSRule&gt;
-      
     &lt;/CORSConfiguration&gt;
 
 3. $ PORT=[PORT] AWS_ACCESS_KEY='[key]' AWS_SECRET_KEY='[secret]' S3_BUCKET='[bucket]' node index.js
--- index.js
+++ index.js
@@ -2,40 +2,57 @@ var express = require('express');
 var body_parser = require('body-parser')
 var http = require('http');
 var path = require('path');
-var aws = require('aws-sdk');
+var crypto = require('crypto');
 var cors = require('cors');
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(body_parser.urlencoded({ extended: true }))
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: true }));
 
 var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-var S3_BUCKET = process.env.S3_BUCKET
+var S3_BUCKET = process.env.S3_BUCKET;
+
+var prefix = 'uploads/';
+var postUrl = 'https://' + S3_BUCKET + '.s3.amazonaws.com/'
 
 app.listen(app.get('port'));
 
 app.get('/sign_s3', cors(), function(req, res){
-    aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
-    var s3 = new aws.S3();
-    var s3_params = {
-        Bucket: S3_BUCKET,
-        Key: req.query.file_name,
-        Expires: 60,
-        ContentType: req.query.content_type,
-        ACL: 'public-read'
-    };
-    s3.getSignedUrl('putObject', s3_params, function(err, data){
-        if(err)
-            console.log(err);
-        else {
-            var return_data = {
-                signed_request: data,
-                url: 'https://' +S3_BUCKET + '.s3.amazonaws.com/' + req.query.file_name
-            };
-            res.write(JSON.stringify(return_data));
-            res.end();
-        }
-    });
+	if (typeof req.query.key !== 'string' || req.query.key.length <= 3)
+		return res.status(400).json({ err: 'bad request' });
+
+	var date = new Date();
+	date.setTime(Date.now() + (30 * 60 * 1000));
+	var isoDate = date.toISOString();
+
+	var s3Policy = {
+    expiration: isoDate,
+    conditions: [
+      ['starts-with', '$key', prefix],
+      {bucket: S3_BUCKET},
+      {acl: 'public-read'},
+      //['starts-with', '$Content-Type', '*']
+    ]
+  };
+
+	// stringify and encode the policy
+  var stringPolicy = JSON.stringify(s3Policy);
+  var base64Policy = Buffer(stringPolicy, 'utf-8').toString('base64');
+
+  // sign the base64 encoded policy
+  var signature = crypto.createHmac('sha1', AWS_SECRET_KEY)
+    .update(new Buffer(base64Policy, 'utf-8')).digest('base64');
+
+
+	res.json({
+		policy: base64Policy,
+		signature: signature,
+		postUrl: postUrl,
+		baseUrl: postUrl + prefix,
+		awsAccessKey: AWS_ACCESS_KEY
+	});
+
 });
